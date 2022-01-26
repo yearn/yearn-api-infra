4dafc2949260221cb29dfbabffc63082e5ce8ff3
numan <numan856@gmail.com>
2022-01-26 15:36:38 -0800

    Initial commit

---
+++ test/yearn-api-infra.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as YearnApiInfra from '../lib/yearn-api-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/yearn-api-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new YearnApiInfra.YearnApiInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
